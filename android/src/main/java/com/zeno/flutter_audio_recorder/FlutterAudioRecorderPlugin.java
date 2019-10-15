c11545bdd7ad1b2d9a5fe9433a59edc8e76e0849
Zhu Tao <zhutao.iscas@gmail.com>
2019-10-15 09:32:28 +0800

    fix the power issue

--- android/src/main/java/com/zeno/flutter_audio_recorder/FlutterAudioRecorderPlugin.java
+++ android/src/main/java/com/zeno/flutter_audio_recorder/FlutterAudioRecorderPlugin.java
@@ -9,19 +9,12 @@ import android.os.SystemClock;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.util.TimeUtils;
 
-import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.flutter.plugin.common.MethodCall;
@@ -98,30 +91,53 @@ public class FlutterAudioRecorderPlugin implements MethodCallHandler {
     }
   }
 
-  private void handleResume(MethodCall call, Result result) {
-    mStatus = "recording";
-    mRecorder.startRecording();
-    result.success(null);
-  }
-
-  private void handlePause(MethodCall call, Result result) {
-    mStatus = "paused";
-    mRecorder.stop();
-    result.success(null);
-  }
-
-  private void handleStop(MethodCall call, Result result) {
-    mStatus = "stopped";
-    mPeakPower = 0;
-    mAveragePower = 0;
-    mRecordingThread = null;
-    mRecorder.stop();
-    mRecorder.release();
-    try {
-      mFileOutputStream.close();
-    } catch (IOException e) {
-      e.printStackTrace();
+  private boolean handleHasPermission(){
+    if(hasRecordPermission()){
+      return true;
     }
+    ActivityCompat.requestPermissions(registrar.activity(), new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+    int i = 0;
+    int TRY_TIMES = 10;
+    while (i < TRY_TIMES){
+      // check the permission
+      SystemClock.sleep(1000);
+      if(hasRecordPermission()){
+        return true;
+      }
+      i += 1;
+    }
+    return false;
+  }
+
+  private void handleInit(MethodCall call, Result result)  {
+    mStartTime = new Date();
+    mSampleRate = Integer.parseInt(call.argument("sampleRate").toString());
+    mFilePath = call.argument("path").toString();
+    mExtension = call.argument("extension").toString();
+    bufferSize = AudioRecord.getMinBufferSize(mSampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+    mStatus = "initialized";
+    HashMap<String, Object> initResult = new HashMap<>();
+    initResult.put("duration", 0);
+    initResult.put("path", mFilePath);
+    initResult.put("audioFormat", mExtension);
+    initResult.put("peakPower", mPeakPower);
+    initResult.put("averagePower", mAveragePower);
+    initResult.put("isMeteringEnabled", true);
+    initResult.put("status", mStatus);
+    result.success(initResult);
+  }
+
+  private void handleCurrent(MethodCall call, Result result) {
+    HashMap<String, Object> currentResult = new HashMap<>();
+    currentResult.put("duration", getDuration());
+    currentResult.put("path", mFilePath);
+    currentResult.put("audioFormat", mExtension);
+    currentResult.put("peakPower", mPeakPower);
+    currentResult.put("averagePower", mAveragePower);
+    currentResult.put("isMeteringEnabled", true);
+    currentResult.put("status", mStatus);
+    Log.d(LOG_NAME, currentResult.toString());
+    result.success(currentResult);
   }
 
   private void handleStart(MethodCall call, Result result) {
@@ -144,16 +160,50 @@ public class FlutterAudioRecorderPlugin implements MethodCallHandler {
     result.success(null);
   }
 
+  private void handlePause(MethodCall call, Result result) {
+    mStatus = "paused";
+    mPeakPower = 0;
+    mAveragePower = 0;
+    mRecorder.stop();
+    result.success(null);
+  }
+
+
+  private void handleResume(MethodCall call, Result result) {
+    mStatus = "recording";
+    mRecorder.startRecording();
+    result.success(null);
+  }
+
+
+  private void handleStop(MethodCall call, Result result) {
+    mStatus = "stopped";
+    mPeakPower = 0;
+    mAveragePower = 0;
+    mRecordingThread = null;
+    mRecorder.stop();
+    mRecorder.release();
+    try {
+      mFileOutputStream.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+
   private void processAudioStream() {
     Log.d(LOG_NAME, "processing the stream");
 //    int BufferElements2Rec = 1024 * 2;
-    short sData[] = new short[bufferSize];
+    int size = bufferSize;
+    short sData[] = new short[size];
 
     while (mStatus == "recording"){
       Log.d(LOG_NAME, "reading audio data");
-      mRecorder.read(sData, 0, bufferSize);
-      byte bData[] = short2byte(sData);
+      mRecorder.read(sData, 0, size);
       updatePowers(sData);
+      byte bData[] = short2byte(sData);
+
         try {
           mFileOutputStream.write(bData, 0, bData.length);
         } catch (IOException e) {
@@ -184,6 +234,7 @@ public class FlutterAudioRecorderPlugin implements MethodCallHandler {
     }
     mPeakPower = max;
     mAveragePower = sum / data.length;
+    Log.d(LOG_NAME, "Peak: " + mPeakPower + " average: "+ mAveragePower);
   }
 
   private int getDuration(){
@@ -191,54 +242,5 @@ public class FlutterAudioRecorderPlugin implements MethodCallHandler {
     long diff = now.getTime() - mStartTime.getTime();
     long duration = TimeUnit.MILLISECONDS.toSeconds(diff);
     return (int)duration;
-  }
-
-  private void handleCurrent(MethodCall call, Result result) {
-    HashMap<String, Object> currentResult = new HashMap<>();
-    currentResult.put("duration", getDuration());
-    currentResult.put("path", mFilePath);
-    currentResult.put("audioFormat", mExtension);
-    currentResult.put("peakPower", mPeakPower);
-    currentResult.put("averagePower", mAveragePower);
-    currentResult.put("isMeteringEnabled", true);
-    currentResult.put("status", mStatus);
-    Log.d(LOG_NAME, currentResult.toString());
-    result.success(currentResult);
-  }
-
-  private void handleInit(MethodCall call, Result result)  {
-    mStartTime = new Date();
-    mSampleRate = Integer.parseInt(call.argument("sampleRate").toString());
-    mFilePath = call.argument("path").toString();
-    mExtension = call.argument("extension").toString();
-    bufferSize = AudioRecord.getMinBufferSize(mSampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-    mStatus = "initialized";
-    HashMap<String, Object> initResult = new HashMap<>();
-    initResult.put("duration", 0);
-    initResult.put("path", mFilePath);
-    initResult.put("audioFormat", mExtension);
-    initResult.put("peakPower", mPeakPower);
-    initResult.put("averagePower", mAveragePower);
-    initResult.put("isMeteringEnabled", true);
-    initResult.put("status", mStatus);
-    result.success(initResult);
-  }
-
-  private boolean handleHasPermission(){
-    if(hasRecordPermission()){
-      return true;
-    }
-    ActivityCompat.requestPermissions(registrar.activity(), new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
-    int i = 0;
-    int TRY_TIMES = 10;
-    while (i < TRY_TIMES){
-      // check the permission
-      SystemClock.sleep(1000);
-      if(hasRecordPermission()){
-        return true;
-      }
-      i += 1;
-    }
-    return false;
   }
 }
