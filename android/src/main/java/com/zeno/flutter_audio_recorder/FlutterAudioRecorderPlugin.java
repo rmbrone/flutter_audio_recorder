f7dd166ca40b061388e778c31d230a60a9b40708
Zhu Tao <zhutao.iscas@gmail.com>
2019-10-15 10:40:22 +0800

    now it can record wav

--- android/src/main/java/com/zeno/flutter_audio_recorder/FlutterAudioRecorderPlugin.java
+++ android/src/main/java/com/zeno/flutter_audio_recorder/FlutterAudioRecorderPlugin.java
@@ -5,11 +5,14 @@ import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.Environment;
 import android.os.SystemClock;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -27,6 +30,7 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 public class FlutterAudioRecorderPlugin implements MethodCallHandler {
   private static final String LOG_NAME = "FlutterAudioRecorder";
   private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 200;
+  private static final byte RECORDER_BPP = 16; // we use 16bit
   private Registrar registrar = null;
   private int mSampleRate = 16000; // 16Khz
   private AudioRecord mRecorder = null;
@@ -143,7 +147,7 @@ public class FlutterAudioRecorderPlugin implements MethodCallHandler {
   private void handleStart(MethodCall call, Result result) {
     mRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC, mSampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
     try {
-      mFileOutputStream = new FileOutputStream(mFilePath);
+      mFileOutputStream = new FileOutputStream(getTempFilename());
     } catch (FileNotFoundException e) {
       result.error("", "cannot find the file", null);
       return;
@@ -188,6 +192,9 @@ public class FlutterAudioRecorderPlugin implements MethodCallHandler {
     } catch (IOException e) {
       e.printStackTrace();
     }
+    Log.d(LOG_NAME, "before adding the wav header");
+    copyWaveFile(getTempFilename(), mFilePath);
+    deleteTempFile();
   }
 
 
@@ -196,16 +203,16 @@ public class FlutterAudioRecorderPlugin implements MethodCallHandler {
     Log.d(LOG_NAME, "processing the stream");
 //    int BufferElements2Rec = 1024 * 2;
     int size = bufferSize;
-    short sData[] = new short[size];
+//    byte sData[] = new short[size];
+    byte bData[] = new byte[size];
 
     while (mStatus == "recording"){
       Log.d(LOG_NAME, "reading audio data");
-      mRecorder.read(sData, 0, size);
-      updatePowers(sData);
-      byte bData[] = short2byte(sData);
-
+      mRecorder.read(bData, 0, size);
+      updatePowers(bData);
+//      byte bData[] = short2byte(sData);
         try {
-          mFileOutputStream.write(bData, 0, bData.length);
+          mFileOutputStream.write(bData);
         } catch (IOException e) {
           e.printStackTrace();
         }
@@ -222,10 +229,109 @@ public class FlutterAudioRecorderPlugin implements MethodCallHandler {
       sData[i] = 0;
     }
     return bytes;
-
   }
 
-  private void updatePowers(short[] data){
+  private void deleteTempFile() {
+    File file = new File(getTempFilename());
+    file.delete();
+  }
+
+  private String getTempFilename() {
+    String filepath = mFilePath + ".temp";
+//    File tempFile = new File(filepath);
+//    if (tempFile.exists())
+//      tempFile.delete();
+    return filepath;
+  }
+
+  private void copyWaveFile(String inFilename, String outFilename) {
+    FileInputStream in = null;
+    FileOutputStream out = null;
+    long totalAudioLen = 0;
+    long totalDataLen = totalAudioLen + 36;
+    long longSampleRate = mSampleRate;
+    int channels = 1;
+    long byteRate = RECORDER_BPP * mSampleRate * channels / 8;
+
+    byte[] data = new byte[bufferSize];
+
+    try {
+      in = new FileInputStream(inFilename);
+      out = new FileOutputStream(outFilename);
+      totalAudioLen = in.getChannel().size();
+      totalDataLen = totalAudioLen + 36;
+
+      WriteWaveFileHeader(out, totalAudioLen, totalDataLen,
+              longSampleRate, channels, byteRate);
+
+      while (in.read(data) != -1) {
+        out.write(data);
+      }
+
+      in.close();
+      out.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void WriteWaveFileHeader(FileOutputStream out, long totalAudioLen,
+                                   long totalDataLen, long longSampleRate, int channels, long byteRate)
+          throws IOException {
+    byte[] header = new byte[44];
+
+    header[0] = 'R'; // RIFF/WAVE header
+    header[1] = 'I';
+    header[2] = 'F';
+    header[3] = 'F';
+    header[4] = (byte) (totalDataLen & 0xff);
+    header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+    header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+    header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+    header[8] = 'W';
+    header[9] = 'A';
+    header[10] = 'V';
+    header[11] = 'E';
+    header[12] = 'f'; // 'fmt ' chunk
+    header[13] = 'm';
+    header[14] = 't';
+    header[15] = ' ';
+    header[16] = 16; // 4 bytes: size of 'fmt ' chunk
+    header[17] = 0;
+    header[18] = 0;
+    header[19] = 0;
+    header[20] = 1; // format = 1
+    header[21] = 0;
+    header[22] = (byte) channels;
+    header[23] = 0;
+    header[24] = (byte) (longSampleRate & 0xff);
+    header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+    header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+    header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+    header[28] = (byte) (byteRate & 0xff);
+    header[29] = (byte) ((byteRate >> 8) & 0xff);
+    header[30] = (byte) ((byteRate >> 16) & 0xff);
+    header[31] = (byte) ((byteRate >> 24) & 0xff);
+    header[32] = (byte) (1); // block align
+    header[33] = 0;
+    header[34] = RECORDER_BPP; // bits per sample
+    header[35] = 0;
+    header[36] = 'd';
+    header[37] = 'a';
+    header[38] = 't';
+    header[39] = 'a';
+    header[40] = (byte) (totalAudioLen & 0xff);
+    header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+    header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+    header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+
+    out.write(header, 0, 44);
+  }
+
+
+  private void updatePowers(byte[] data){
     double max=0;
     double sum=0;
     for(int i=0; i<data.length; i++){
